8c04878e646e609725d6704b43832ddf0d42450d
Mikita Isakau <Mikita_Isakau@epam.com>
2017-06-02 18:42:39 +0300

    HistoryList -> 2 spaces

--- src/App/components/HistoryList.js
+++ src/App/components/HistoryList.js
@@ -5,33 +5,33 @@ import Element from './Element';
 function mapStateToProps(state, dispatch) {
   return {
     previous: state.previous,
-		dispatch
+    dispatch
   }
 }
 
 class HistoryList extends Component {
 
-	constructor(props){
-		super(props);
-		this.clearHistory = this.clearHistory.bind(this);
-	}
+  constructor(props){
+    super(props);
+    this.clearHistory = this.clearHistory.bind(this);
+  }
 
-	clearHistory() {
-		this.props.dispatch({ type: 'CLEAR_ALL_RECENT' });
-	}
+  clearHistory() {
+    this.props.dispatch({ type: 'CLEAR_ALL_RECENT' });
+  }
 
-	render() {
-		const clearButton = <button onClick = { this.clearHistory }>Clear history</button>;
-		return (
-			<div>
-				<h3>{this.props.previous.length ? 'Recent converts:' : null }</h3>
-				<ul>
-					{ this.props.previous.map( (item, i) => <Element li key={i} convert={item}/> ) }
-				</ul>
-				{ this.props.previous.length ? clearButton : null }
-			</div>
-		);
-	}
+  render() {
+    const clearButton = <button onClick = { this.clearHistory }>Clear history</button>;
+    return (
+      <div>
+        <h3>{this.props.previous.length ? 'Recent converts:' : null }</h3>
+        <ul>
+          { this.props.previous.map( (item, i) => <Element li key={i} convert={item}/> ) }
+        </ul>
+        { this.props.previous.length ? clearButton : null }
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(HistoryList);
